f68b9245f5fecb13e43778d216d89644390c6e16
OlegGlo <olegglotov99@gmail.com>
2024-12-26 19:51:33 -0500

    waitress added for hosting

--- app.py
+++ app.py
@@ -2,12 +2,13 @@ from flask import Flask, jsonify
 import os
 import json
 from flask_cors import CORS
+from waitress import serve  # Import Waitress
 
 app = Flask(__name__)
 CORS(app)
 
 """
-Get todays videos
+Get today's videos
 """
 @app.route("/api/videos/today", methods=["GET"])
 def get_videos_today():
@@ -21,11 +22,11 @@ def get_videos_today():
     with open(data_file_path, "r", encoding="utf-8") as f:
         videos_list = json.load(f)  # This will be a list of dictionaries
     
-    # wrap a dict
+    # Wrap a dict
     return jsonify({"videos": videos_list})
 
 """
-Get todays videos
+Get archive videos
 """
 @app.route("/api/videos/archive", methods=["GET"])
 def get_videos_archive():
@@ -34,20 +35,19 @@ def get_videos_archive():
     data_file_path = os.path.join("DB", "old_videos.json")
     
     if not os.path.exists(data_file_path):
-        return jsonify({"error": "No data available for today."}), 404
+        return jsonify({"error": "No archive data available."}), 404
     
     with open(data_file_path, "r", encoding="utf-8") as f:
         videos_list = json.load(f)  # This will be a list of dictionaries
     
-    # wrap a dict
+    # Wrap a dict
     return jsonify({"videos": videos_list})
 
-
-# tests
+# Test route
 @app.route("/")
 def index():
     return "Flask service is up."
 
 if __name__ == "__main__":
-    # Run Flask in debug mode (for development)
-    app.run(debug=True, host="0.0.0.0", port=5050)
+    # Serve the app using Waitress
+    serve(app, host="0.0.0.0", port=5050)
