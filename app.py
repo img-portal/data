6c10f2fc7c3602646a8d69fe658d677db89df540
OlegGlo <olegglotov99@gmail.com>
2025-01-01 18:33:11 -0500

    api logging

--- app.py
+++ app.py
@@ -21,6 +21,8 @@ def get_videos_today():
     
     with open(data_file_path, "r", encoding="utf-8") as f:
         videos_list = json.load(f)  # This will be a list of dictionaries
+
+    print("fetch daily vids")
     
     # Wrap a dict
     return jsonify({"videos": videos_list})
@@ -39,6 +41,8 @@ def get_videos_archive():
     
     with open(data_file_path, "r", encoding="utf-8") as f:
         videos_list = json.load(f)  # This will be a list of dictionaries
+
+    print("fetch archive vids")
     
     # Wrap a dict
     return jsonify({"videos": videos_list})
